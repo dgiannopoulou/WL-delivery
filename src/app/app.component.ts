4d4e1d32857e29e4e460a2a9748e4a0553c9afe3
korinaxa <korina.xa13@gmail.com>
2024-01-15 20:56:03 +0200

    Added store components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { RestaurantBannerComponent } from './restaurant-banner/restaurant-banner.component';
+import { RestaurantMenuComponent } from './restaurant-menu/restaurant-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomepageComponent],
+  imports: [CommonModule, RouterOutlet, HomepageComponent, RestaurantBannerComponent, RestaurantMenuComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/services/stores.service.ts
+++ src/app/services/stores.service.ts
@@ -8,11 +8,16 @@ export class StoresService {
 
   http = inject(HttpClient);
 
-  url = '/assets/data/stores.json'
+  url_store = '/assets/data/store.json'
+  url_stores = '/assets/data/stores.json'
   url_famous = '/assets/data/most_famous_stores_in_general.json'
 
+  getStore(){
+    return this.http.get(this.url_store);
+  } 
+
   getStores(){
-    return this.http.get(this.url);
+    return this.http.get(this.url_stores);
   }
 
   getFamousStores(){
