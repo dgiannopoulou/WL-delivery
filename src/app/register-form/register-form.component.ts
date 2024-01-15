1217aeab2a13e6362e6d82d58063e21b69749bac
dgiannopoulou <dgiannopoulou@gmail.com>
2024-01-15 19:28:36 +0200

    Login and Register modal created

--- src/app/register-form/register-form.component.ts
+++ src/app/register-form/register-form.component.ts
@@ -1,12 +1,44 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
+
+  form! : FormGroup;
+
+  ngOnInit(){
+    this.setFormValues();
+  }
+
+  setFormValues(){
+    this.form = new FormGroup({
+      firstname: new FormControl(),
+      lastname : new  FormControl(),
+      phone : new FormControl("", Validators.required),
+      password : new FormControl("",Validators.required)
+    });
+  }
+
+  submit(){
+    console.log("hello");
+    console.log(this.form);
+    
+  }
+
+  register(){
+    console.log("register");
+    console.log(this.form);
+  }
+
+  login(){
+    console.log("login");
+    console.log(this.form);
+  }
 
 }
