48e09ff969ae599d915261dd01b239d9c588b984
Marialena <marialena.alevizaki@gmail.com>
2024-01-15 20:10:30 +0200

    Added Error 404 Page - Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { Error404Component } from './error404/error404.component';
 
 export const routes: Routes = [
 
-    {path: 'home', component:HomepageComponent},
-    {path: '', redirectTo: 'home', pathMatch: 'full'},
-    
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    {path: "home", component:HomepageComponent},
+    {path: "**", component: Error404Component}
+
 ];
