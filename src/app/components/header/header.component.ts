cd42c9e5c9dd84ad20625e4a2db5a8a9ffb8d246
dgiannopoulou <dgiannopoulou@gmail.com>
2024-01-22 18:06:49 +0200

    Logi icon and profile menu created

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { RegisterFormComponent } from '../register-form/register-form.component';
+import { ProfileComponent } from '../profile/profile.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchBarComponent,RegisterFormComponent],
+  imports: [SearchBarComponent,RegisterFormComponent, ProfileComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
