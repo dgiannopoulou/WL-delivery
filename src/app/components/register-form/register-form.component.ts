897c529f1eb8a28cc2b35c7b8e8adac449cfc1f9
dgiannopoulou <dgiannopoulou@gmail.com>
2024-01-22 05:17:27 +0200

    Register validation

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
+  
 
   form! : FormGroup;
 
@@ -18,21 +20,39 @@ export class RegisterFormComponent {
 
   setFormValues(){
     this.form = new FormGroup({
-      username: new FormControl(),
-      email : new  FormControl(),
-      phone : new FormControl("", Validators.required),
-      password : new FormControl("",Validators.required)
+      email : new  FormControl("", [Validators.required, Validators.email]),
+      phone : new FormControl("", [Validators.required, Validators.pattern ('[- +()0-9]{10,12}')]),
+      password : new FormControl("",[
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(40)
+      ])
     });
   }
 
+
   register(){
-    console.log("register");
+    if(this.form.valid){
+      // service backend call
+      console.log("valid register!");
+    } else {
+      // error message
+      this.form.markAllAsTouched();
+      console.log("invalid register");
+    }
     console.log(this.form);
   }
 
   login(){
-    console.log("login");
+    if(this.form.valid){
+      // service backend call
+      console.log("valid login!");
+    } else {
+      // error message
+      this.form.markAllAsTouched();
+      console.log("invalid login");
+    }
     console.log(this.form);
   }
+  }
 
-}
