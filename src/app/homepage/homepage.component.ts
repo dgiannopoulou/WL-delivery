4d4a58b0131ac15cec7411177a2ae6eb26950742
Marialena <marialena.alevizaki@gmail.com>
2024-01-22 19:27:14 +0200

    Added Sidebar

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CategoriesComponent } from '../categories/categories.component';
 import { StoresComponent } from '../stores/stores.component';
 import { FamousStoresComponent } from '../famous-stores/famous-stores.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CategoriesComponent, StoresComponent, FamousStoresComponent],
+  imports: [CategoriesComponent, StoresComponent, FamousStoresComponent, SidebarComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
