1e46285b5420aa132f42f8da2118c28a649da509
dgiannopoulou <dgiannopoulou@gmail.com>
2024-01-24 00:44:33 +0200

    acount sidebar menu added

---
+++ src/app/components/account-menu/account-menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './account-menu.component.html',
+  styleUrl: './account-menu.component.css'
+})
+export class AccountMenuComponent {
+
+}
--- src/app/components/account/account.component.ts
+++ src/app/components/account/account.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AccountMenuComponent } from '../account-menu/account-menu.component';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [],
+  imports: [AccountMenuComponent],
   templateUrl: './account.component.html',
   styleUrl: './account.component.css'
 })
