3ece2dba503ad34f0a10efef895a77ac3b3dfcdb
dgiannopoulou <dgiannopoulou@gmail.com>
2024-01-23 15:40:04 +0200

    dropdown menu links added

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { RegisterFormComponent } from '../register-form/register-form.component';
 import { ProfileComponent } from '../profile/profile.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchBarComponent,RegisterFormComponent, ProfileComponent],
+  imports: [SearchBarComponent,RegisterFormComponent, ProfileComponent,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
