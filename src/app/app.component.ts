fcd93274d057a9ff1989e0574ddea983111e9aea
Veroniki Atart <veronikiatart@gmail.com>
2024-01-24 19:14:52 +0200

    check error on form console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,13 +7,14 @@ import { RestaurantMenuComponent } from './restaurant-menu/restaurant-menu.compo
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RegisterFormComponent } from './components/register-form/register-form.component';
+import { CheckoutPageComponent } from "./checkout-page/checkout-page.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HomepageComponent, RestaurantBannerComponent, RestaurantMenuComponent, HeaderComponent, FooterComponent, RegisterFormComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [CommonModule, RouterOutlet, HomepageComponent, RestaurantBannerComponent, RestaurantMenuComponent, HeaderComponent, FooterComponent, RegisterFormComponent, CheckoutPageComponent]
 })
 export class AppComponent {
   title = 'WL-delivery';
--- src/app/checkout-page/checkout-page.component.ts
+++ src/app/checkout-page/checkout-page.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-checkout-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './checkout-page.component.html',
   styleUrl: './checkout-page.component.css'
 })
 export class CheckoutPageComponent {
-  
+  form!: FormGroup;
+
+  ngOnInIt(){
+this.setFormValues();
+
+  }
+
+  setFormValues(){
+this.form = new FormGroup({
+  firstName: new FormControl("", Validators.required),
+  lastName: new FormControl("", Validators.required),
+  username: new FormControl("", Validators.required),
+  email: new  FormControl("", [Validators.required, Validators.email]),
+  address: new FormControl("", Validators.required),
+  zip: new FormControl("", Validators.required)
+});
+
+  }
+
+  onSubmit(){
+    if(this.form.valid){
+console.log("valid!");
+
+    } else {
+      console.log("invalid");
+
+    }
+    
+    console.log(this.form);
+    
+  }
 }
