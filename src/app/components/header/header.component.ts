085debf780e5f4184f91de998e79ecc01eccc9a2
meffraim <meffraim@gmail.com>
2024-01-27 14:29:55 +0200

    changes in header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { RegisterFormComponent } from '../register-form/register-form.component';
-import { ProfileComponent } from '../profile/profile.component';
+import { ProfileAvatarComponent } from '../profile-avatar/profile-avatar.component';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchBarComponent,RegisterFormComponent, ProfileComponent],
+  imports: [SearchBarComponent, RegisterFormComponent, ProfileAvatarComponent, CommonModule, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
