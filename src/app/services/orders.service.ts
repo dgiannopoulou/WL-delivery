bced79ae86de588969720344383e338b94f899fb
dgiannopoulou <dgiannopoulou@gmail.com>
2024-01-27 02:00:12 +0200

    complete detail order component

--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -9,13 +9,15 @@ export class OrdersService {
 
   http = inject(HttpClient)
   url = '/assets/data/my_orders.json'
+  urlDetails = '/assets/data/my_orders_{id}.json'
+
 
   getOrders() {
     return this.http.get(this.url);
   }
 
-  getOrderById(id: number) {
-    return this.http.get(this.url + "/" + id);
+  getOrderById(id: string) {
+    return this.http.get( this.urlDetails.replace("{id}",id) );
   }
 
   constructor() { }
