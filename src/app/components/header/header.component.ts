a237e8e4decf3ee7246bb344f6adfce2450eddfb
meffraim <meffraim@gmail.com>
2024-01-27 14:12:27 +0200

    component categorys changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { RegisterFormComponent } from '../register-form/register-form.component';
 import { ProfileComponent } from '../profile/profile.component';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchBarComponent,RegisterFormComponent, ProfileComponent],
+  imports: [RouterLink, SearchBarComponent,RegisterFormComponent, ProfileComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
