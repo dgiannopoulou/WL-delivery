836b12e939cd457a399d0d3e67877cd9aa7dca30
Marialena <marialena.alevizaki@gmail.com>
2024-01-29 20:32:24 +0200

    sidebar changes

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CategoriesService } from '../services/categories.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
+  categories: any;
+
+  service = inject(CategoriesService);
+
+  ngOnInit(): void {
+    this.getCategoriesData()
+  }
+
+  getCategoriesData() {
+    this.service.getCategories().subscribe(
+      {
+        next: response => this.categories = response
+      }
+    )
+  }
 }
