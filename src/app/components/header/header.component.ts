6f5ee6fec6c401aa22150be9554e689b7a20e312
dgiannopoulou <dgiannopoulou@gmail.com>
2024-01-29 02:02:03 +0200

    hide cart

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,45 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { RegisterFormComponent } from '../register-form/register-form.component';
 import { ProfileAvatarComponent } from '../profile-avatar/profile-avatar.component';
 import { CommonModule } from '@angular/common';
 import { CartStatusComponent } from '../../cart-status/cart-status.component';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchBarComponent, 
-            RegisterFormComponent, 
-            ProfileAvatarComponent,
-            RouterLink, 
-            CommonModule,  
-            CartStatusComponent],
+  imports: [SearchBarComponent,
+    RegisterFormComponent,
+    ProfileAvatarComponent,
+    RouterLink,
+    CommonModule,
+    CartStatusComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
 
+  router: Router = inject(Router)
+
   loggedIn?: boolean;
-  userLog?:boolean;
-  searchBar? :boolean;
+  userLog?: boolean;
+  searchBar?: boolean;
+  cartStatus?: boolean;
 
   loggedUser(data: boolean) {
     this.loggedIn = data;
   }
 
   ngOnInit() {
-    let url = window.location.href.toString();
-    console.log(url);
-    if (url.includes('account')|| url.includes('checkout')) {
-      this.searchBar = false;
-    }else{
-      this.searchBar = true;
-    }
-    
-    this.userLog = localStorage.getItem('logIn') === 'true' ;
-    
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        let url = window.location.href.toString();
+        console.log(url);
+
+        if (url.includes('account') || url.includes('checkout')) {
+          this.searchBar = false;
+        } else {
+          this.searchBar = true;
+        }
+
+        if (url.includes('account') || url.includes('checkout') || url.includes('home')) {
+          this.cartStatus = false;
+        } else {
+          this.cartStatus = true;
+        }
+
+
+        this.userLog = localStorage.getItem('logIn') === 'true';
+      }
+    });
   }
 }
 
