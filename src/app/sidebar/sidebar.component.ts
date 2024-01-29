faa5657e39c9aa3d496f2254606fa5dbe788a234
Marialena <marialena.alevizaki@gmail.com>
2024-01-29 20:38:29 +0200

    extra changes to sidebar

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { CategoriesService } from '../services/categories.service';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
